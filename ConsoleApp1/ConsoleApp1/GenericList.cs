358e7762bfa5fff79045fc1addfec145cbfc1a43
karin <karin@DESKTOP-G3TCD3F>
2019-07-01 21:43:20 +0300

    Done

---
+++ ConsoleApp1/ConsoleApp1/GenericList.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class GenericList<T> : IEnumerable<T> where T : IComparable
+    {
+        private const string CapacityMinimumValueErrorMessage = "Non-negative number required";
+        private const string IndexOutOfRangeErrorMessage = "Index was out of range. Must be non-negative and less than the size of the collection";
+        private const string ToStringItemsSeparator = ", ";
+        private const int CapacityMinimumValue = 0;
+        private const int DefaultInitialCapacity = 4;
+        private const int InitialItemsCount = 0;
+        private const int GrowCoefficient = 2;
+
+        private int capacity;
+        private T[] items;
+
+        public GenericList() : this(GenericList<T>.DefaultInitialCapacity) { }
+
+        public GenericList(int capacity)
+        {
+            this.Capacity = capacity;
+            this.Count = GenericList<T>.InitialItemsCount;
+            this.items = new T[this.Capacity];
+        }
+
+        public int Capacity
+        {
+            get
+            {
+                return this.capacity;
+            }
+            set
+            {
+                if (value < CapacityMinimumValue)
+                {
+                    throw new ArgumentOutOfRangeException("capacity", GenericList<T>.CapacityMinimumValueErrorMessage);
+                }
+
+                this.capacity = value;
+            }
+        }
+
+        public int Count { get; private set; }
+
+        public T this[int index]
+        {
+            get
+            {
+                this.ValidateIndex(index);
+
+                return this.items[index];
+            }
+            set
+            {
+                this.ValidateIndex(index);
+
+                this.items[index] = value;
+            }
+        }
+
+        public void Add(T item)
+        {
+            if (this.Count == this.Capacity)
+            {
+                this.ResizeItemsCollection();
+            }
+
+            this.items[this.Count] = item;
+            this.Count++;
+        }
+
+        public void RemoveAt(int index)
+        {
+            this.ValidateIndex(index);
+
+            for (int position = index; position < this.Count - 1; position++)
+            {
+                this.items[position] = this.items[position + 1];
+            }
+
+            this.items[this.Count - 1] = default(T);
+            this.Count--;
+        }
+
+        public void InsertAt(int index, T item)
+        {
+            if (this.Count == this.Capacity)
+            {
+                this.ResizeItemsCollection();
+            }
+
+            for (int position = this.Count + 1; position > index; position--)
+            {
+                this.items[position] = this.items[position - 1];
+            }
+
+            this.items[index] = item;
+            this.Count++;
+        }
+
+        public void Clear()
+        {
+            this.Capacity = GenericList<T>.DefaultInitialCapacity;
+            this.Count = GenericList<T>.InitialItemsCount;
+            this.items = new T[this.Capacity];
+        }
+
+        public T Min()
+        {
+            int index = 0;
+
+            T min = this.items[index];
+
+            for (; index < this.Count; index++)
+            {
+                if (this.items[index].CompareTo(min) < 0)
+                {
+                    min = this.items[index];
+                }
+            }
+
+            return min;
+        }
+
+        public T Max()
+        {
+            int index = 0;
+
+            T max = this.items[index];
+
+            for (; index < this.Count; index++)
+            {
+                if (this.items[index].CompareTo(max) > 0)
+                {
+                    max = this.items[index];
+                }
+            }
+
+            return max;
+        }
+
+        public int IndexOf(T item)
+        {
+            for (int index = 0; index < this.Count; index++)
+            {
+                if (this.items[index].CompareTo(item) == 0)
+                {
+                    return index;
+                }
+            }
+
+            return -1;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int item = 0; item < this.Count; item++)
+            {
+                yield return items[item];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+
+            for (int item = 0; item < this.Count; item++)
+            {
+                result.Append(this.items[item]);
+
+                if (item < this.Count - 1)
+                {
+                    result.Append(GenericList<T>.ToStringItemsSeparator);
+                }
+            }
+
+            return result.ToString();
+        }
+
+        private void ValidateIndex(int index)
+        {
+            if (index < 0 || index >= this.Count)
+            {
+                throw new ArgumentOutOfRangeException("index", GenericList<T>.IndexOutOfRangeErrorMessage);
+            }
+        }
+
+        private void ResizeItemsCollection()
+        {
+            this.Capacity *= GrowCoefficient;
+
+            T[] newItems = new T[this.Capacity];
+
+            Array.Copy(this.items, newItems, this.Count);
+
+            this.items = newItems;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Generic List
+            GenericList<int> genericList = new GenericList<int>();
+
+            genericList.Add(12);
+            genericList.Add(3);
+            genericList.Add(6);
+            genericList.Add(32);
+            genericList.Add(8);
+
+            Console.WriteLine("Generic list ToString(): {0}", genericList);
+
+            int index = 3;
+            genericList.RemoveAt(index);
+
+            Console.WriteLine("Generic list after removing element at index [{0}]: {1}", index, genericList);
+
+            int element = 7;
+            genericList.InsertAt(index, element);
+
+            Console.WriteLine("Generic list after inserting element {0} at index [{1}]: {2}", element, index, genericList);
+
+            Console.WriteLine("Index of element {0}: {1}", element, genericList.IndexOf(element));
+
+            Console.WriteLine("Generic list count: {0}", genericList.Count);
+            Console.WriteLine("Generic list capacity: {0}", genericList.Capacity);
+
+            Console.WriteLine("Min element: {0}", genericList.Min());
+            Console.WriteLine("Max element: {0}", genericList.Max());
+
+
+            //Matrix
+
+            int rows = 3;
+            int cols = 3;
+            int cellValue = 1;
+
+            Matrix<int> firstMatrix = new Matrix<int>(rows, cols);
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    firstMatrix[row, col] = cellValue;
+                    cellValue++;
+                }
+            }
+
+            Matrix<int> secondMatrix = new Matrix<int>(rows, cols);
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    cellValue--;
+                    secondMatrix[row, col] = cellValue;
+
+                }
+            }
+
+            Console.WriteLine("First matrix:");
+            Console.WriteLine(firstMatrix);
+
+            Console.WriteLine("First matrix: {0}\n", firstMatrix ? "non-empty" : "empty");
+
+            Console.WriteLine("Second matrix:");
+            Console.WriteLine(secondMatrix);
+
+            Console.WriteLine("First matrix + Second matrix: ");
+            Console.WriteLine(firstMatrix + secondMatrix);
+
+            Console.WriteLine("First matrix - Second matrix: ");
+            Console.WriteLine(firstMatrix - secondMatrix);
+
+            Console.WriteLine("First matrix * Second matrix: ");
+            Console.WriteLine(firstMatrix * secondMatrix);
+
+            for (int row = 0; row < firstMatrix.Rows; row++)
+            {
+                for (int col = 0; col < firstMatrix.Columns; col++)
+                {
+                    firstMatrix[row, col] = 0;
+                }
+            }
+
+            Console.WriteLine("First matrix: ");
+            Console.WriteLine(firstMatrix);
+
+            Console.WriteLine("First matrix: {0}\n", firstMatrix ? "non-empty" : "empty");
+        }
+    }
+}
